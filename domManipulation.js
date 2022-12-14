4626fd107ed0f87baab32ee074c02be121a3ec94
Reese Marshall <reese.amarshall@gmail.com>
2022-12-14 11:27:44 -0500

    Completed my solution

--- domManipulation.js
+++ domManipulation.js
@@ -2,7 +2,7 @@
  * Step 1 - Declare a variable called "petList".
  * Step 2 - Use .querySelector() to search for the element with the id of 'pet-list' and assign it as the value of the "petList" variable.
  */
-
+const petList = document.querySelector('#pet-list')
 /*
  * Your code goes here
  */
@@ -11,19 +11,19 @@
  * Step 1 - Declare a variable called "newPet".
  * Step 2 - Use .createElement() to create a new li element and assign it as the value of the "newPet" variable.
  */
-
+const newPet = document.createElement('li');
 /*
  * Your code goes here
  */
 
 // Deliverable # 3: .textContent - Change the textContent of "newPet" to any pet of your choosing (for example: "Guinea Pig")
-
+newPet.textContent = "Guinea Pig";
 /*
  * Your code goes here
  */
 
 // Deliverable # 4: .appendChild() - Use .appendChild() to append "newPet" (the new li element you created) to "petList" (the ol containing all of the pets).
-
+petList.appendChild(newPet);
 /*
  * Your code goes here
  */
@@ -32,6 +32,11 @@
  * Step 1 - Declare a variable called "divs".
  * Step 2 - Use .querySelectorAll() to search for all div elements and assign it as the value of the "divs" variable.
  */
+
+/*
+ * Your code goes here
+ */
+const divs = document.querySelectorAll("div");
 
 /*
  * Your code goes here
@@ -46,6 +51,14 @@
  * Step 6 - On the next line, inside of the function body of the callback function, use .appendChild() to append "copyrightElement" (the new h2 element you created) to "div" (the argument of the callback function representing each div element contained within the "divs" variable).
  */
 
+    divs.forEach(callbackArgument);
+    function callbackArgument(div){
+        const copyrightElement = document.createElement("h2");
+        copyrightElement.textContent = "© Reese Marshall";
+        div.appendChild(copyrightElement);
+    }
+
+
 /*
  * Your code goes here
  */
@@ -54,7 +67,7 @@
  * Step 1 - Declare a variable called "notAShop".
  * Step 2 - Use .getElementById() to search for the element with the id of 'not-a-shop' and assign it as the value of the "notAShop" variable.
  */
-
+const notAShop = document.getElementById('not-A-Shop');
 /*
  * Your code goes here
  */
@@ -62,7 +75,7 @@
 /* Deliverable # 8: .remove()
  * Step 1 - Use .remove() to remove the element with the id of 'not-a-shop' from the DOM. You already have a variable named "notAShop" that references this element.
  */
-
+notAShop.remove();
 /*
  * Your code goes here
  */
@@ -71,7 +84,7 @@
  * Step 1 - Declare a variable called "shops".
  * Step 2 - Use .getElementsByClassName() to search for all elements with the class of "shop" and assign it as the value of the "shops" variable.
  */
-
+const shops = document.getElementsByClassName("shops");
 /*
  * Your code goes here
  */
@@ -85,7 +98,13 @@
  *          For example, if "counter" is 0, then shops[counter] is equivalent to shops[0]. Use appendChild() to append the button to each of the shops.
  * Step 6 - Don't forget to increment the value of the "counter" on the last line inside of the while loop!
  */
-
+let counter = 0;
+while (counter < shops.length) {
+    const button = document.createElement("button");
+    button.textcontent = "Like This Shop";
+    shops[counter].appendChild(button);
+    counter++;
+}
 /*
  * Your code goes here
  */
